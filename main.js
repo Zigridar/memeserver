bd5f980ef5bbca41eecc8ec0e7af526de118ee1f
zigridar <safonenko.max@yandex.ru>
2020-05-16 18:02:14 +0500

    IEP Diplom

--- main.js
+++ main.js
@@ -2,28 +2,57 @@
 
 const PORT = 3000;
 
-const server = require('http').createServer((req, res) => {
-    res.end('Socket-server is running');
-});
+const express = require('express')
+const bodyParser = require('body-parser')
 
-const io = require('socket.io')(server);
-server.listen(PORT);
+const app = express()
 
-io.on('connection', socket => {
+app.listen(3000, () => {
+    console.log(`Server start up on port: ${PORT}`)
+})
 
-    console.log('connection')
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/src/html/index.html')
+    console.log('New connection ' + new Date())
+})
 
-    socket.on('offer', (offer) => {
-        socket.emit('answer', 'data')
-        console.log('offer');
-    });
 
-    socket.on('answer', (answer, id) => {
-        console.log('answer')
-    });
+app.use('/js/index.js', (req, res) => {
+    res.sendFile(__dirname + '/src/js/index.js')
+})
 
-    socket.on('disconnect', () => {
-        console.log('disconnect')
-    });
+app.use('/L.jpeg', (req, res) => {
+    res.sendFile(__dirname + '/src/img/L.jpeg')
+})
 
-});
+app.use('/shit.png', (req, res) => {
+    res.sendFile(__dirname + '/src/img/shit1.png')
+})
+
+app.use(bodyParser.json())
+
+app.post('/', (req, res) => {
+    console.log(req.body)
+    console.log(req.headers)
+    // res.end(JSON.stringify(req.body))
+    res.end("kek")
+})
+
+// io.on('connection', socket => {
+//
+//     console.log('connection')
+//
+//     socket.on('offer', (offer) => {
+//         socket.emit('answer', 'data')
+//         console.log('offer');
+//     });
+//
+//     socket.on('answer', (answer, id) => {
+//         console.log('answer')
+//     });
+//
+//     socket.on('disconnect', () => {
+//         console.log('disconnect')
+//     });
+//
+// });
